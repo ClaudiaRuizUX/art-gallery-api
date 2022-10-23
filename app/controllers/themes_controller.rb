44b923c218ca99aa1d8d8c4c28ac8fd0603f09d4
ClaudiaRuizUX <claudiaruizux@gmail.com>
2022-10-24 07:06:52 +1100

    creates service class and configures serializer, bug theme as null

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,6 +1,11 @@
 class ThemesController < ApplicationController
     def index
         themes = Theme.all
-        render json: themes, include: [:project]
+        render json: ThemingSerializer.new(themes).to_serialized_json
+    end
+
+    def show
+        theme = Theme.find_by(id: params[:id])
+        render json: ThemingSerializer.new(theme).to_serialized_json
     end
 end
