e460b918f52ac84c7a4ed3d84117bc07dc82aff3
ClaudiaRuizUX <claudiaruizux@gmail.com>
2022-10-27 07:50:04 +1100

    Initial commit for art-app

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+project_a = Project.create(title: "Trees", description: "Green of spring")
+project_b = Project.create(title: "Circles", description: "Geometric circle")
+project_c = Project.create(title: "Apples", description: "Many Apples")
+project_d = Project.create(title: "Squares", description: "Some Squares")
